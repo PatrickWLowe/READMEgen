0d966666de84c3fe49a0e005004ff5d1b0ce7b1e
Pat Lowe <lowe-patrick@att.net>
2023-08-27 17:51:45 -0700

    fixed some functionality bug

--- index.js
+++ index.js
@@ -1,95 +1,91 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const GenMD = require('./utils/generateMarkdown');
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const GenMD = require("./utils/generateMarkdown");
 
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What would You like your Project Title to be?',
+  {
+    type: "input",
+    name: "title",
+    message: "What would You like your Project Title to be?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a Description of your Project?",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: 'Please add any information to your "Installation" Section?',
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: 'Please add any information to your "Usage" Section?',
+  },
+  {
+    type: "list",
+    name: "License",
+    message: 'Would You like to add a "License"?',
+    choices: ["MIT", "Apache 2.0", "IBM Public License Version 1.0"],
+    filter(choice) {
+      return choice;
     },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide a Description of your Project?',
-    },
-    {
-        type: 'input',
-        name: 'Installation',
-        message: 'Please add any information to your "Installation" Section?',
-    },
-    {
-        type: 'input',
-        name: 'Usage',
-        message: 'Please add any information to your "Usage" Section?',
-    },
-    {
-        type: 'list',
-        name: 'License',
-        message: 'Would You like to add a "License"?',
-        choices: ['The MIT License', 'Apache 2.0', 'IBM Public License Version 1.0'],
-        filter(choice) {
-           return choice; 
-        }
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Please add any information to your "Contributing" Section?',
-    },
-    {
-        type: 'input',
-        name: 'Tests',
-        message: 'Please add any information to your "Tests" Section?',
-    },
-    {
-        type: 'input',
-        name: 'QuestionsSec',
-        message: 'Please add any information to your "Questions" Section?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    },
-    {
-        type: 'input',
-        name: 'Github',
-        message: 'What is your github username?',
-    },
-    
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: 'Please add any information to your "Contributing" Section?',
+  },
+  {
+    type: "input",
+    name: "Tests",
+    message: 'Please add any information to your "Tests" Section?',
+  },
+  {
+    type: "input",
+    name: "QuestionsSec",
+    message: 'Please add any information to your "Questions" Section?',
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "Github",
+    message: "What is your github username?",
+  },
 ];
 
-
-const writeToFile = Filecontent => {
-    fs.writeFile('README.md', Filecontent, err => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('Your README.md file has been created')
-    });
+const writeToFile = (Filecontent) => {
+  fs.writeFile("README.md", Filecontent, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Your README.md file has been created");
+  });
 };
 
 function init() {
-    return inquirer.prompt(questions)
-    .then((data) => {
-        return data;
-    }) 
-};
+  return inquirer.prompt(questions).then((data) => {
+    return data;
+  });
+}
 
 init()
-.then(data => {
+  .then((data) => {
     console.log(data);
     return GenMD(data);
-})
-.then(pageMD => {
+  })
+  .then((pageMD) => {
     return writeToFile(pageMD);
-})
-.then(writeFileResponse => {
+  })
+  .then((writeFileResponse) => {
     console.log(writeFileResponse);
-})
-.catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
